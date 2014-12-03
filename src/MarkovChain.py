ac730794570e1cc8a5348e85982119779ad513b3
System Administrator <root@Philipps-MacBook-Pro.local>
2014-12-03 17:30:28 +0100

    changed

--- src/MarkovChain.py
+++ src/MarkovChain.py
@@ -26,6 +26,7 @@ from scipy.sparse import csr_matrix, coo_matrix
 import itertools
 import copy
 import tables as tb
+import warnings
 
 RESET_STATE = "-1"
 
@@ -56,28 +57,28 @@ class MarkovChain():
         '''
         self.k_ = k
         self.reset_ = reset
-        
+
         self.state_count_initial_ = 0
         self.states_initial_ = []
         self.parameter_count_ = 0
         self.observation_count_ = 0
-        
+
         self.paths_ = list()
         self.paths_test_ = list()
-        
+
         #probabilities
         self.transition_dict_ = defaultdict(lambda : defaultdict(float))
-        
+
         self.prediction_position_dict_ = dict()
         self.vocabulary_ = None
         self.state_distr_ = defaultdict(float)
-        
+
         self.states_ = dict()
         self.states_reverse_ = dict()
         self.dtype_ = np.dtype(float)
         self.reverse_ = reverse
         self.modus_ = modus
-        
+
         self.use_prior_ = use_prior
         self.prior_ = prior
 
@@ -86,7 +87,7 @@ class MarkovChain():
         self.specific_prior_ = specific_prior
         self.specific_prior_vocab_ = specific_prior_vocab
 
-        
+
         #print self.specific_prior_
         if self.specific_prior_ is not None and k != 1:
             raise Exception("Using specific priors with higher orders not yet implemented!")
@@ -94,11 +95,13 @@ class MarkovChain():
             raise Exception("Can't work with a specific prior without vocabulary information!")
         if self.specific_prior_ is not None and self.modus_ != "bayes":
             raise Exception("Specific prior only works mit Bayes modus!")
+        if self.specific_prior_ is not None and isinstance(self.specific_prior_, csr_matrix) and self.specific_prior_.shape[0] != self.specific_prior_.shape[1]:
+            warnings.warn("Specific prior dimensions are not the same. Only appropriate if one the matrix is 1xN for setting each row the same!")
 
         self.proba_from_unknown_ = 0
         self.proba_to_unknown_ = dict()
 
-    def _dict_divider(self, d): 
+    def _dict_divider(self, d):
         '''
         Internal function for dict divider and smoothing
         '''
@@ -108,12 +111,12 @@ class MarkovChain():
             print "smoothing divider: ", smoothing_divider
             self.proba_from_unknown_ = self.prior_ / smoothing_divider
             print "proba_from_unknown_: ", self.proba_from_unknown_
-            
+
             for k, v in d.iteritems():
                 s = float(sum(v.values()))
                 #smoothing_divider = float(sum([round(x*self.alpha_)+self.prior_ for x in self.specific_prior_[k].values()]))
                 #smoothing_divider += float((self.state_count_initial_ - len(self.specific_prior_[k].values())) * self.prior_)
-                
+
                 divider = s + smoothing_divider
                 self.observation_count_ += divider
                 for i, j in v.iteritems():
@@ -126,9 +129,9 @@ class MarkovChain():
                 self.observation_count_ += s
                 for i, j in v.iteritems():
                     v[i] = j / s
-                    
+
                 #print "row sum: ", float(sum(v.values()))
-                
+
     def _dict_ranker(self, d):
         '''
         Apply ranks to a dict according to the values
@@ -137,8 +140,8 @@ class MarkovChain():
         my_d = collections.defaultdict(list)
         for key, val in d.items():
             my_d[val].append(key)
-        
-        ranked_key_dict = {} 
+
+        ranked_key_dict = {}
         n = v = 1
         for _, my_list in sorted(my_d.items(), reverse=True):
             #v = n + (len(my_list)-1)/2.
@@ -146,14 +149,14 @@ class MarkovChain():
             for e in my_list:
                 n += 1
                 ranked_key_dict[e] = v
-                
+
         #little hack for storing the other unobserved average ranks
         #this is wanted so that we do not have to calculate it all the time again
         #ranked_key_dict[FAKE_ELEM] = n + ((self.state_count_initial_-len(ranked_key_dict)-1)/2.)
         ranked_key_dict[FAKE_ELEM] = self.state_count_initial_
 
         return ranked_key_dict
-        
+
     def prepare_data(self, paths):
         '''
         preparing data
@@ -161,17 +164,17 @@ class MarkovChain():
         '''
         states = set()
         if self.reset_:
-            states.add(RESET_STATE)    
-        
+            states.add(RESET_STATE)
+
         for line in paths:
             for ele in line:
                 states.add(ele)
                 self.state_distr_[ele] += 1
-                
+
         #print self.state_distr_
-                
+
         self.states_initial_ = frozenset(states)
-                
+
         sum_state_occ = sum(self.state_distr_.values())
         for k,v in self.state_distr_.iteritems():
             self.state_distr_[k] = float(v) / float(sum_state_occ)
@@ -181,7 +184,7 @@ class MarkovChain():
         self.parameter_count_ = pow(self.state_count_initial_, self.k_) * (self.state_count_initial_ - 1)
         print "initial state count", self.state_count_initial_
         #print self.states_initial_
-        
+
     def fit(self, paths, ret=False):
         '''
         fitting the data and constructing MLE
@@ -190,13 +193,13 @@ class MarkovChain():
         print "====================="
         print "K: ", self.k_
         print "prior: ", self.prior_
-        
+
         for line in paths:
             if self.reset_:
                 self.paths_.append(self.k_*[RESET_STATE] + [x for x in line] + [RESET_STATE])
             else:
                 self.paths_.append([x for x in line])
-        
+
         for path in self.paths_:
             i = 0
             for j in xrange(self.k_, len(path)):
@@ -207,20 +210,20 @@ class MarkovChain():
                     self.transition_dict_[FAKE_ELEM][elemB] += 1
                 else:
                     self.transition_dict_[elemA][elemB] += 1
-        
+
         #print self.transition_dict_
 
-        
+
         if self.modus_ == "mle":
             self._dict_divider(self.transition_dict_)
 
         if ret:
             return self.transition_dict_
-            
+
         #sys.exit()
 
 
-        
+
     def loglikelihood(self):
         '''
         Calculating the log likelihood of the fitted MLE
@@ -231,7 +234,7 @@ class MarkovChain():
 
         likelihood = 0
         prop_counter = 0
-        
+
         for path in self.paths_:
             i = 0
             for j in xrange(self.k_, len(path)):
@@ -240,11 +243,11 @@ class MarkovChain():
                 elemB = path[j]
                 if self.k_ == 0:
                     prop = self.transition_dict_[FAKE_ELEM][elemB]
-                else: 
+                else:
                     prop = self.transition_dict_[elemA][elemB]
                 likelihood += math.log(prop)
                 prop_counter += 1
-                            
+
         print "likelihood", likelihood
         print "prop_counter", prop_counter
         return likelihood
@@ -259,9 +262,9 @@ class MarkovChain():
         '''
         if self.modus_ != "bayes":
             raise Exception("Bayesian evidence does not work with modus='mle'")
-        
+
         print "starting to do bayesian evidence calculation!!"
-        
+
         evidence = 0
 
         counter = 0
@@ -286,30 +289,32 @@ class MarkovChain():
                 #print tmp
 
             first_term_enum = 0
-            first_term_denom = 0        
+            first_term_denom = 0
             second_term_enum = 0
             second_term_denom = 0
 
             #start with combining prior knowledge with real data
 
-            if self.specific_prior_ is not None and k[0] != RESET_STATE:
-                if isinstance(self.specific_prior_, csr_matrix):
-                    cx = self.specific_prior_.getrow(self.specific_prior_vocab_[k[0]])
-                elif isinstance(self.specific_prior_, tb.group.RootGroup):
-                    row = self.specific_prior_vocab_[k[0]]
-                    indptr_first = self.specific_prior_.indptr[row]
-                    indptr_second = self.specific_prior_.indptr[row+1]
-                    data = self.specific_prior_.data[indptr_first:indptr_second]
-                    indices = self.specific_prior_.indices[indptr_first:indptr_second]
-                    indptr = np.array([0,indices.shape[0]])
-                    if self.reset_:
-                        shape = (1, self.state_count_initial_-1)
+            if self.specific_prior_.shape[0] == self.specific_prior_.shape[1]:
+                if self.specific_prior_ is not None and k[0] != RESET_STATE:
+                    if isinstance(self.specific_prior_, csr_matrix):
+                        cx = self.specific_prior_.getrow(self.specific_prior_vocab_[k[0]])
+                    elif isinstance(self.specific_prior_, tb.group.RootGroup):
+                        row = self.specific_prior_vocab_[k[0]]
+                        indptr_first = self.specific_prior_.indptr[row]
+                        indptr_second = self.specific_prior_.indptr[row+1]
+                        data = self.specific_prior_.data[indptr_first:indptr_second]
+                        indices = self.specific_prior_.indices[indptr_first:indptr_second]
+                        indptr = np.array([0,indices.shape[0]])
+                        if self.reset_:
+                            shape = (1, self.state_count_initial_-1)
+                        else:
+                            shape = (1, self.state_count_initial_)
+                        cx = csr_matrix((data, indices, indptr), shape=shape)
                     else:
-                        shape = (1, self.state_count_initial_)
-                    cx = csr_matrix((data, indices, indptr), shape=shape)
-                else:
-                    raise Exception("wrong specific prior format")
-                
+                        raise Exception("wrong specific prior format")
+            else:
+                cx = self.specific_prior_
 
             done = set()
             done_counter = 0
