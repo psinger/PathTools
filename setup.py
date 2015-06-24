bef36dcc3e2b695e2e60bad5b5e72134f36ea51a
Philipp Singer <philipp.singer@gesis.org>
2015-06-24 13:39:20 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from setuptools import find_packages
 setup(
     name='pathtools',
     version='0.5',
+    author='Philipp Singer',
+    author_email='philipp.singer@gesis.org',
     packages=['pathtools'],
     license='MIT License',
+    url='https://github.com/psinger/PathTools',
 )
