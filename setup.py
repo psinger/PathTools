908fe8a6c614b4fffdeceeecd21051d7d33ff1da
unknown <singerpp@KOL15028.gesis.intra>
2015-06-24 12:41:07 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+__author__ = 'singerpp'
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='pathtools',
+    version='0.5',
+    packages=['pathtools'],
+    license='MIT License',
+)
